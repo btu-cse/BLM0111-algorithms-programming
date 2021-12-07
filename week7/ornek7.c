b2c148ba77ab4d485e24d05976662c2c89584ce5
Sena <48378319+senadikici@users.noreply.github.com>
2021-12-07 15:20:50 +0300

    Create ornek7.c

---
+++ week7/ornek7.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str1[100];
+    char str2[100];
+    int result;
+
+    strcpy(str1,"hello");
+    strcpy(str2,"Hello");
+
+    result=strcmp(sayi1,sayi2);
+
+    if (result==0) {
+    printf("Str1 ve Str2 stringi birbirine esittir");
+    }
+    else {
+    printf("Str1 ve Str2 stringi birbirine esit degildir.");
+    }
+
+    return 0;
+}
