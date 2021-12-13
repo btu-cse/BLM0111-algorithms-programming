ec6914a3d6da593bf3e5087c7b41e48def0db8d4
Ahmet Kaşif <ksfahmet@gmail.com>
2021-12-13 10:08:45 +0300

    Create recursive1.c

---
+++ week9/recursive1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+unsigned long long int factorial(unsigned int i);
+
+int  main() {
+   int i = 12;
+   printf("Factorial of %d is %d\n", i, factorial(i));
+   return 0;
+}
+
+unsigned long long int factorial(unsigned int i) {
+
+   if(i <= 1) {
+      return 1;
+   }
+   return i * factorial(i - 1);
+}
