490678ac03fc30ed7c1e5ca1f1994a7e86167dff
Sena <48378319+senadikici@users.noreply.github.com>
2021-12-20 11:33:17 +0300

    Create ornek2.c

---
+++ week10/ornek2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void func(int *x){
+   for(int i = 0; i < 4; ++i){
+      printf("&x[%d] = %u, value: %d\n", i, &*(x+i), *(x+i));
+   }
+}
+
+int main()
+{
+   int x[4]={1,3,5,7};
+
+   func(x);
+
+   return 0;
+}
